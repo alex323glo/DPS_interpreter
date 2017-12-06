71af5c9b5e080dd2151710aa2ab1c674c0dc18e2
alex323glo <alex323glo@gmail.com>
2017-12-07 00:29:41 +0200

    Updated Interpreter interface and its implementation (InterpreterImpl class).

--- src/main/java/com/alex323glo/dps/interpreter/Interpreter.java
+++ src/main/java/com/alex323glo/dps/interpreter/Interpreter.java
@@ -4,10 +4,30 @@ import com.alex323glo.dps.interpreter.exception.InterpreterException;
 import com.alex323glo.dps.interpreter.model.state.State;
 import com.alex323glo.dps.parser.exception.ParserException;
 
-// TODO add doc
+/**
+ * Interface for DPS Interpreter, which parses and executes DPS Commands
+ * on DPS (script) State - crates new DPS State.
+ *
+ * @author alex323glo
+ * @version 1.0.0
+ */
 public interface Interpreter {
 
-    // TODO fix logic of interpret()
+    /**
+     * Interprets (executes) DPS Commands on initial DPS State.
+     *
+     * @param state initial DPS (script) State.
+     * @param commandsStr String representation of DPS Commands, which will be interpreted (executed).
+     * @return new DPS (script) State, safe for old State (doesn't change old object and creates new one).
+     * @throws ParserException when in some reasons can't parse DPS Commands (from String to
+     * List of Command class objects).
+     * @throws InterpreterException when in some reasons can't interpret (execute) parsed DPS Commands
+     * on initial DPS (script) State.
+     *
+     * @see State
+     * @see ParserException
+     * @see InterpreterException
+     */
     State interpret(State state, String commandsStr) throws ParserException, InterpreterException;
 
 }
--- src/main/java/com/alex323glo/dps/interpreter/InterpreterImpl.java
+++ src/main/java/com/alex323glo/dps/interpreter/InterpreterImpl.java
@@ -1,54 +1,93 @@
 package com.alex323glo.dps.interpreter;
 
 import com.alex323glo.dps.interpreter.exception.InterpreterException;
+import com.alex323glo.dps.interpreter.model.config.Config;
+import com.alex323glo.dps.interpreter.model.config.DefaultConfig;
 import com.alex323glo.dps.parser.CommandParser;
 import com.alex323glo.dps.parser.exception.ParserException;
 import com.alex323glo.dps.parser.model.command.Command;
 import com.alex323glo.dps.interpreter.model.state.State;
-import com.dima.dps.parser.Parser;
-import com.dima.dps.parser.exceptions.ParserError;
-import com.dima.dps.parser.parser_commands.ExecutionCommand;
 
 import java.util.List;
 
 /**
- * TODO add doc
+ * Implementation of Interpreter interface. Uses CommandParser to
+ * parse DPS Commands from String representation.
+ *
+ * @author alex323glo
+ * @version 1.0.0
+ *
+ * @see Interpreter
+ * @see CommandParser
  */
 public class InterpreterImpl implements Interpreter {
 
-    // TODO adapt to v1.Interpreter interface
+    // TODO fix DEFAULT_CONFIG field
+    private static final Config DEFAULT_CONFIG = new DefaultConfig();
+
     private CommandParser commandParser;
 
+    /**
+     * Initial constructor.
+     *
+     * @param commandParser initial field value.
+     *
+     * @see CommandParser
+     * */
     public InterpreterImpl(CommandParser commandParser) {
         this.commandParser = commandParser;
     }
 
-//    public String interpret(String state, String commands) throws InterpreterException {
-//        // Parse commands:
-//        List<ExecutionCommand> commandList;
-//        try {
-//            commandList = parseCommands(commands);
-//        } catch (ParserError pe) {
-//            pe.printStackTrace();
-//            throw new InterpreterException("ParserError was occured");
-//        }
-//
-//        // Async interpretation:
-//        String newState = branchInterpretation(state, commandList);
-//
-//        return newState;
-//    }
+    /**
+     * Interprets (executes) DPS Commands on initial DPS State.
+     * Uses CommandParser to parse (from String) DPS Commands before execution.<br>
+     *
+     * Uses branchInterpretation() method to turn DPS Commands interpretation process to
+     * parallel (branched) way.
+     *
+     * @param state       initial DPS (script) State.
+     * @param commandsStr String representation of DPS Commands, which will be interpreted (executed).
+     * @return new DPS (script) State, safe for old State (doesn't change old object and creates new one).
+     * @throws ParserException      when in some reasons can't parse DPS Commands (from String to
+     *                              List of Command class objects).
+     * @throws InterpreterException when in some reasons can't interpret (execute) parsed DPS Commands
+     *                              on initial DPS (script) State.
+     *
+     * @see InterpreterImpl#branchInterpretation(State, List)
+     * @see State
+     * @see ParserException
+     * @see InterpreterException
+     */
+    @Override
+    public State interpret(State state, String commandsStr) throws ParserException, InterpreterException {
+        if (state == null || commandsStr == null) {
+            throw new NullPointerException("state or commandStr is null");
+        }
+
+        List<Command> commandList = parseCommands(commandsStr);   // throws ParserException !
+
+        State resultState = branchInterpretation(state, commandList);   // throws InterpreterException !
+
+        if (resultState == null) {
+            throw new InterpreterException(
+                    String.format("can't interpret commands (%s) on initial state (%s)", commandsStr, state));
+        }
+
+        return resultState;
+    }
 
     /**
-     * Uses Parser to parse String commands.
+     * Uses CommandParser to parse String commands to List of Command class objects.
      *
-     * @param commands text of commands.
-     * @return list of Command objects.
+     * @param commands String representation of group of commands.
+     * @return List of Command class objects.
      *
-     * @throws ParserException when Parser can't parse text of commands.
+     * @throws ParserException when Parser can't parse group of commands from
+     * String to List of Command class objects.
      *
-     * @see ExecutionCommand
-     * @see ParserError
+     * @see Command
+     * @see List
+     * @see ParserException
      * */
     private List<Command> parseCommands(String commands) throws ParserException {
 
@@ -62,29 +101,64 @@ public class InterpreterImpl implements Interpreter {
     }
 
     /**
-     * Turns interpretation process to async branched way.
+     * Turns interpretation process (execution of DPS Commands on DPS State)
+     * to parallel (branched) process. <br>
+     *
+     * Uses executeBranch() method to invoke
+     * execution of each branch (one branch for one command).
      *
      * @param state start state of DPS script.
      * @param commands list of commands, which execution will be branched.
-     * @return result state after execution of all branches.
+     * @return result state after execution of all branches (commands).
      *
-     * @see ExecutionCommand
+     * @see InterpreterImpl#executeBranch(State, Command)
+     * @see Command
+     * @see State
      * */
-    private String branchInterpretation(String state, List<ExecutionCommand> commands) {
+    private State branchInterpretation(State state, List<Command> commands) throws InterpreterException {
+
+        if (state == null || commands == null) {
+            throw new NullPointerException("state or commands is null");
+        }
+
+        State oldState = state;
 
         // TODO replace with parallel
+        for (Command command: commands) {
+            state = executeBranch(state, command);  // throws InterpreterException !
+        }
 
         return state;
     }
 
-    @Override
-    public State interpret(State state, String commandsStr) throws ParserException, InterpreterException {
-        if (state == null || commandsStr == null) {
-            throw new NullPointerException("state or commandStr is null");
+    /**
+     * Executes single DPS Command on initial DPS State as
+     * parallel thread function.
+     *
+     * @param state initial DPS State.
+     * @param command DPS Command, which will be executed.
+     * @return new DPS State (safe for old DPS State), created after DPS Command execution.
+     * @throws InterpreterException when in some reasons DPS Command can't be executed and/or
+     * new DPS State can't be created.
+     *
+     * @see Command
+     * @see State
+     * @see InterpreterException
+     */
+    private State executeBranch(State state, Command command) throws InterpreterException {
+
+        if (state == null || command == null) {
+            throw new NullPointerException("state or command is null");
         }
 
-        List<Command> commandList = parseCommands(commandsStr);   // throws ParserException
+        // TODO replace DEFAULT_CONFIG with real config
+        State resultState = command.execute(state, DEFAULT_CONFIG);
 
-        return null;
+        if (resultState == null) {
+            throw new InterpreterException(
+                    String.format("can't execute command (%s) on state (%s)", command, state));
+        }
+
+        return resultState;
     }
 }
